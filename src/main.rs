70c6fe6520b982d884598e6d79a210564587e333
Marcin Bigos <marcin.bigos99@gmail.com>
2026-05-12 22:41:03 +0200

    example token to test github protection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::io::stdin;
+use std::io::{self, stdin};
 
 //struct Message {}
 
 fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
+
+    input = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30".to_string();
     println!("{}", input);
 }
