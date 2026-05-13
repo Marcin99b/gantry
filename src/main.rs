879cbafc37baabc4e782256ef7bc12e1e14236d1
Marcin Bigos <marcin.bigos99@gmail.com>
2026-05-13 15:55:25 +0200

    put and get in same file

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
 //struct Message {}
 
 fn main() {
+    let file_path = "data.txt";
     loop {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
@@ -14,22 +15,22 @@ fn main() {
         let body = splitted_input.next().unwrap();
         match method {
             "put" => {
-                put(body);
+                put(body, file_path);
             }
             "get" => {
-                get(body);
+                get(body, file_path);
             }
             _ => panic!("Command unknown"),
         }
     }
 }
 
-fn put(command: &str) {
+fn put(command: &str, file_path: &str) {
     println!("Method: put; Data: {}", command);
     let mut buffer = OpenOptions::new()
         .append(true)
         .create(true)
-        .open("data.txt")
+        .open(file_path)
         .unwrap();
 
     buffer
@@ -37,10 +38,10 @@ fn put(command: &str) {
         .unwrap();
 }
 
-fn get(command: &str) {
+fn get(command: &str, file_path: &str) {
     println!("Method: get; Data: {}", command);
     let offset = command.parse::<usize>().unwrap();
-    let mut buffer = File::open("data.txt").unwrap();
+    let mut buffer = File::open(file_path).unwrap();
     let mut buf = String::new();
     buffer.read_to_string(&mut buf).unwrap();
     let line = buf.lines().nth(offset).unwrap();
