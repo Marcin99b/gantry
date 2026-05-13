d54ec2751308dc71611606da250620034d908934
Marcin Bigos <marcin.bigos99@gmail.com>
2026-05-13 12:58:48 +0200

    display put and get commands

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-use std::io::{self, stdin};
+use std::io::stdin;
 
 //struct Message {}
 
 fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
+    let mut splitted_input = input.split_whitespace();
+    let method = splitted_input.next().unwrap();
+    let body = splitted_input.next().unwrap();
+    match method {
+        "put" => {
+            put(body);
+        }
+        "get" => {
+            get(body);
+        }
+        _ => panic!("Command unknown"),
+    }
+}
 
-    println!("{}", input);
+fn put(command: &str) {
+    println!("put: {}", command);
+}
+
+fn get(command: &str) {
+    println!("get: {}", command);
 }
