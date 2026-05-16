62f293d849217143ab731ebcaeefc3604e75b9bd
Marcin Bigos <marcin.bigos99@gmail.com>
2026-05-16 18:38:46 +0200

    working put

--- src/commands/put_message.rs
+++ src/commands/put_message.rs
@@ -1,3 +1,15 @@
+use std::{fs::OpenOptions, io::Write};
+
 use crate::commands::models::Command;
 
-pub fn handle(command: Command) {}
+pub fn handle(command: Command) {
+    let mut buffer = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("data.txt")
+        .unwrap();
+
+    buffer.write_all(&command.data).unwrap();
+
+    buffer.write_all("\r\n\r\n\r\n".as_bytes()).unwrap();
+}
